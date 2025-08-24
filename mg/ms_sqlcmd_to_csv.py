e518e2b7bb1425057d945bbe048744a99e464969
karaks79 <AntinariT@yandex.ru>
2025-08-25 02:15:46 +0300

    2025-08-25_1

---
+++ mg/ms_sqlcmd_to_csv.py
@@ -0,0 +1,26 @@
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from pendulum import datetime
+
+with DAG(
+    dag_id="ms_sqlcmd_to_csv",
+    description="Выгрузка данных в csv-файл",
+    schedule=None,	
+    start_date=datetime(2025, 8, 24, 8, 0, 0, 0, tz='Europe/Moscow'),
+    catchup=False,
+    tags=['mg'],
+) as dag:
+
+    t1 = BashOperator(
+    task_id='t1',
+    bash_command=“sqlcmd -S {{ conn.mssql_olap_main.host }} -d {{ conn.mssql_olap_main.schema }} -U {{ conn.mssql_olap_main.login }} -P {{ conn.mssql_olap_main.password }} -Q ‘SET NOCOUNT ON; SELECT order_id, order_guid, order_numder, phone FROM Angstrem.mgtest.phone’ -W -s’;’ -u -o ‘C:\Users\M.Grapenyuk\Documents\mg\test\file_phone_unicode.csv’”,
+    )
+
+    t2 = BashOperator(
+    task_id='t2',
+    bash_command=“powershell -Command ‘Get-Content C:\Users\M.Grapenyuk\Documents\mg\test\file_phone_unicode.csv | Set-Content C:\Users\M.Grapenyuk\Documents\mg\test\file_phone_utf8.csv -Encoding utf8’”,
+    )
+
+t1 >> t2
