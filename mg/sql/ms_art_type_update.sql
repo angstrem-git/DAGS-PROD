7a3d8787674e25b49294e889082e5118f975d1eb
karaks79 <AntinariT@yandex.ru>
2025-09-24 23:12:15 +0300

    2025-09-24

--- mg/sql/ms_art_type_update.sql
+++ mg/sql/ms_art_type_update.sql
@@ -18,15 +18,24 @@ WHERE
 			[mg2].[art] AS ar
 			LEFT JOIN [catalog].[nomenclature] AS nom ON ar.[nomenclature_id] = nom.[nomenclature_id]
 			LEFT JOIN [catalog].[nomenclature_property] AS prop_nom ON ar.[nomenclature_property_id] = prop_nom.[nomenclature_property_id]
+			LEFT JOIN [mg2].[art_type] AS atp ON ar.[art_id] = atp.[art_id]
 		WHERE 
 			ar.[nomenclature_id] IN (   
 									48461		-- Комплект фасадов индивидуального заказа
 									,142238		-- Индивидуальный заказ Кухня
 									,160059		-- Набор универсально-сборной мебели для кухни
 									)
-			AND (  
-				prop_nom.[nomenclature_property_name] LIKE 'АЛХ%' 
-				OR prop_nom.[nomenclature_property_name] LIKE 'ЯМ3%' 
-				OR prop_nom.[nomenclature_property_name] LIKE 'ЯМ4%' 
-				)
+			AND 
+			-- mg-24.09.2025 -----------------------------------------------------------------
+				--(  
+				--prop_nom.[nomenclature_property_name] LIKE 'АЛХ%' 
+				--OR prop_nom.[nomenclature_property_name] LIKE 'ЯМ3%' 
+				--OR prop_nom.[nomenclature_property_name] LIKE 'ЯМ4%' 
+				--)
+			-- mg-24.09.2025 -----------------------------------------------------------------
+				prop_nom.[nomenclature_property_name] LIKE '%[0-9]/[0-9]%[0-9]'	-- Самолет
+			AND 
+				atp.[type_id] <> 2
+			-- mg-24.09.2025 -----------------------------------------------------------------
+
 		)
