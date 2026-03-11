08202af3fb613d6e43db3c07657aeee910887f63
karaks79 <AntinariT@yandex.ru>
2026-03-12 00:29:48 +0300

    2026-03-11_12

--- mg/rasp_pipeline/rasp_v01/py/v01_t02_check_sum.py
+++ mg/rasp_pipeline/rasp_v01/py/v01_t02_check_sum.py
@@ -33,8 +33,7 @@ def check_sum(RELEASE_key, DAG_DIR_key, DB1_key, URL_key, USER_key, PASSWORD_key
 
     #r.raise_for_status()
     if r.status_code != 200:
-        print(r.text)
-        raise Exception(r.text)
+        raise Exception(f"ClickHouse error: {r.text}")
 
     d = r.json()["data"][0]["total_check"]
 
