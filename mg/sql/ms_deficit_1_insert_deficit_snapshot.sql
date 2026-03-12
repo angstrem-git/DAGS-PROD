019160cd13b14aab4d17dc7c32d1af71452769ea
karaks79 <AntinariT@yandex.ru>
2026-03-12 20:49:28 +0300

    2026-03-12_03

--- mg/sql/ms_deficit_1_insert_deficit_snapshot.sql
+++
@@ -1,3 +0,0 @@
--- mg, 2024-07-06
--- Заполнение таблицы-шапки.
-INSERT INTO [mg2].[deficit_snapshot] DEFAULT VALUES
--- mg/sql/ms_deficit_3_bulk_insert_csv_sost_reserva_po_paketam.sql
+++
@@ -1,5 +0,0 @@
--- mg, 2024-07-07
--- Импорт файла данных в таблицу базы данных 
-BULK INSERT [mg1].[csv_sost_reserva_po_paketam]
-FROM 'C:\Data\mg\sost_reserva_po_paketam.txt'
-WITH (fieldterminator = '\t', rowterminator = '\n', datafiletype = 'widechar');   -- '\t' - табуляция
