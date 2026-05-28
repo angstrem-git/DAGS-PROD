e55374cb1c5c8eadd7538d4971eae747b79ee383
karaks79 <antinarit@gmail.com>
2026-05-28 03:51:26 +0300

    2026-05-27_13

--- mg/rasp_test_2026-05-26/rasp_test_2026-05-26.py
+++ mg/rasp_test_2026-05-26/rasp_test_2026-05-26.py
@@ -168,7 +168,7 @@ def get_batch_id_dttm(**context):
     
     context["ti"].xcom_push(
         key="date_id",
-        value=str((date_id)
+        value=str(date_id)
         )
     
     return True
